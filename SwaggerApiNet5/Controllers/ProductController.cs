b3ea26b3de001b755c02b9cbd8eeaccefb362a03
Jose Lucio Perez Tovar <sistemas25@revuelta.com.mx>
2021-05-17 13:02:46 -0500

    Add project files.

---
+++ SwaggerApiNet5/Controllers/ProductController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SwaggerApiNet5.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public IActionResult Products() => Ok(new { Id = 1, Name = "Smartphone" });
+    }
+}
---
+++ XUnitTest/UnitTest1.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SwaggerApiNet5.Controllers;
+using Xunit;
+
+namespace XUnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var controller = new ProductController();
+            var result = controller.Products();
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            if (okResult.Value == null) return;
+            var json = JsonConvert.SerializeObject(okResult.Value);
+            var values = JsonConvert.DeserializeObject<Product>(json);
+            if (values != null) Assert.Equal(1, values.Id);
+        }
+        public class Product
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
